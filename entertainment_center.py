3dad80cce837f1b8171d833f00e0e9c3be9ef04e
IAmHaxk <32108399+IAmHaxk@users.noreply.github.com>
2017-09-19 18:55:05 -0700

    Main Entertainment file

---
+++ entertainment_center.py
@@ -0,0 +1,13 @@
+import media
+import fresh_tomatoes
+
+war_games = media.Movie("WarGames","Is it a game, or is it real?","https://images-na.ssl-images-amazon.com/images/M/MV5BMTMyMTE3OTk3NF5BMl5BanBnXkFtZTcwOTkwNDc3NA@@._V1_SY1000_CR0,0,666,1000_AL_.jpg","https://youtu.be/hbqMuvnx5MU")
+take_down= media.Movie("TakeDown","The world has a right to know","http://www.freewebs.com/w6csa/hackers2jn5.jpg","https://youtu.be/NbgDMYy9mzM")
+hackers = media.Movie("Hackers","HACK THE PLANET!","https://www.mauvais-genres.com/18552/hackers-movie-poster-29x40-in-1995-iain-softley-angelina-jolie.jpg","https://youtu.be/Rn2cf_wJ4f4")
+Live_Free = media.Movie("Live Free Or Die Hard","Live free, or Die Hard!","https://www.filmlinks4u.is/wp-content/uploads/2015/12/Live-Free-or-Die-Hard-2007-Telugu-Dubbed-Movie-Watch-Online.jpg","https://youtu.be/Sv8Uh2Usldo")
+blackhat = media.Movie("blackhat","You are no longer in control","https://bloximages.newyork1.vip.townnews.com/the-standard.org/content/tncms/assets/v3/editorial/9/19/919e9612-a02d-11e4-98cc-f7f843c3e5fe/54bd8a57211ca.image.jpg?resize=500%2C747","https://youtu.be/jZ1ZDlLImF8")
+snowden = media.Movie("Snowden","The only safe place is on the run","https://cdn3.clickthecity.com/images/movies/poster/600/15500.jpg","https://youtu.be/QlSAiI3xMh4")
+
+
+movies = [war_games,take_down,hackers,Live_Free,blackhat,snowden]
+
